84c93f197e6eda5cf1c69acdc21042e624e6328a
Baray Ann <annbaray66@gmail.com>
2024-10-20 16:18:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-text=open('text.txt', encoding="utf-8")   
-print("Количество слов в файле:", len(text.read().split()))
-text.close()
+text=open('text.txt', encoding="utf-8") #Открытие файла с помощью кодировки utf-8 и присваивание его переменной text
+print("Количество слов в файле:", len(text.read().split())) #Вывод на консоль количества слов в файле с помощью функции len() и функции разделения на элементы split()
+text.close() #Закрытие файла
